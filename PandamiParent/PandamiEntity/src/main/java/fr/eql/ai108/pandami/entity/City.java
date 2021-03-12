c6861fde3bdd60463a64f372ebbceff402a4998d
Floriane <floriane.ahadi@gmail.com>
2021-03-12 15:05:56 +0100

    ajout toString(), hashCode et equals ds entity. modif Account (business et mb)

--- PandamiParent/PandamiEntity/src/main/java/fr/eql/ai108/pandami/entity/City.java
+++ PandamiParent/PandamiEntity/src/main/java/fr/eql/ai108/pandami/entity/City.java
@@ -30,6 +30,48 @@ public class City implements Serializable {
 		this.name = name;
 		this.zipCode = zipCode;
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "City [id=" + id + ", name=" + name + ", zipCode=" + zipCode + "]";
+	}
 
 	public Integer getId() {
 		return id;
@@ -55,5 +97,5 @@ public class City implements Serializable {
 		this.zipCode = zipCode;
 	}
 
-
+	
 }
