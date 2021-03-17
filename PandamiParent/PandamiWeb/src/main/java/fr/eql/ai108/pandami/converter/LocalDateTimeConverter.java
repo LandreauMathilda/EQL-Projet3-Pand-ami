a83098154cb8c6ad48b229006b54ba3e9b87ed7f
Kevin <lennoz.kevin@hotmail.fr>
2021-03-17 10:11:00 +0100

    ajout Converters

---
+++ PandamiParent/PandamiWeb/src/main/java/fr/eql/ai108/pandami/converter/LocalDateTimeConverter.java
@@ -0,0 +1,27 @@
+package fr.eql.ai108.pandami.converter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter(value="localDateTimeConverter")
+public class LocalDateTimeConverter implements Converter {
+ 
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+          return LocalDateTime.parse(value);
+    }
+ 
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+ 
+        LocalDateTime dateValue = (LocalDateTime) value;
+
+        return dateValue.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+    }
+
+}
